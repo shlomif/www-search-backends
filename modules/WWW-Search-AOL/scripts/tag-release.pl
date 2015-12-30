fb634c88ac51e89da899343e5a30bf0d3d692fd3
Shlomi Fish <shlomif@shlomifish.org>
2015-12-30 15:06:02 +0200

    add tag-release.

---
+++ modules/WWW-Search-AOL/scripts/tag-release.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use IO::All;
+
+my ($version) =
+    (map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
+    io->file("./lib/WWW/Search/AOL.pm")->getlines()
+    )
+    ;
+
+if (!defined ($version))
+{
+    die "Version is undefined!";
+}
+
+my @cmd = (
+    "hg", "tag", "-m",
+    "Tagging WWW-Search-AOL as $version",
+    "releases/WWW-Search-AOL/$version",
+);
+
+print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
+exec(@cmd);
